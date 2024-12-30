d5ca5a2073bdb85e9c57009057050cf67e11172c
Christian Danner <christian.danner@hotmail.com>
2024-12-29 21:09:02 -0300

    Inserida tarefa para exibição no LCD das variáveis das outras duas tarefas
    O sistema está funcionando com 3 tarefas simultaneamente, mas no limite da memória SRAM, utilizando um total de 718 bytes.

--- FreeRTOS-Sistema-de-Controle-Supervisorio-Arduino.ino
+++ FreeRTOS-Sistema-de-Controle-Supervisorio-Arduino.ino
@@ -13,7 +13,6 @@
 #include <DHT.h>
 #include <Arduino_FreeRTOS.h>
 
-
 // Configurações do display LCD
 LiquidCrystal_I2C lcd(0x27, 16, 2);  // Endereço I2C do LCD
 
@@ -22,242 +21,42 @@ LiquidCrystal_I2C lcd(0x27, 16, 2);  // Endereço I2C do LCD
 #define DHTTYPE DHT11
 DHT dht(DHTPIN, DHTTYPE);
 
-// Pinos dos botões
-#define BTN_UP 8
-#define BTN_DOWN 7
-#define BTN_SELECT 3
-#define BTN_BACK 2
+// Pino potenciômetro
+#define potPin A0
 
-// Pinos do relé e LEDs
-#define RELE_PIN 10
-#define LED_VERMELHO 11
-#define LED_AMARELO 12
-bool releState = false;
-bool ledVermelhoState = false;
-bool ledAmareloState = false;
-bool backlightState = true;
-
-// Servo
-//Servo meuServo;   // Cria um objeto Servo para controlar o servo motor
-//int pos;          // Variável para armazenar a posição do servo motor
-//int potPin = A0;  // Pino analógico onde o potenciômetro está conectado
-//int val;          // Variável para armazenar o valor lido do potenciômetro
-
-// Menu
-int menuIndex = 0;
-const char *menuOptions[] = {
-  "1. Pot./Servo",
-  "2. Temp./Umidade",
-  "3. Rele On/Off",
-  "4. Msg no LCD",
-  "5. LED Vermelho",
-  "6. LED Amarelo",
-  "7. Backlight"
-};
-const int menuSize = sizeof(menuOptions) / sizeof(menuOptions[0]);
+// Variáveis globais compartilhadas
+volatile int pos;
+volatile int val;
+volatile float temp;
+volatile float umid;
 
 void setup() {
 
   // Inicializa o monitor serial
-  Serial.begin(9600);
+  //Serial.begin(9600);
 
-  //lcd.init();
-  //lcd.noBacklight();
-  //lcd.noDisplay();
-  //dht.begin();
-
-  //pinMode(RELE_PIN, OUTPUT);
-  //pinMode(LED_VERMELHO, OUTPUT);
-  //pinMode(LED_AMARELO, OUTPUT);
-  //digitalWrite(RELE_PIN, LOW);
-  //digitalWrite(LED_VERMELHO, LOW);
-  //digitalWrite(LED_AMARELO, LOW);
-
-  // Configuração dos botões
-  //pinMode(BTN_UP, INPUT_PULLUP);
-  //pinMode(BTN_DOWN, INPUT_PULLUP);
-  //pinMode(BTN_SELECT, INPUT_PULLUP);
-  //pinMode(BTN_BACK, INPUT_PULLUP);
-
-  //meuServo.attach(6);  // Associa o servo motor ao pino digital 6 do Arduino
-  //euServo.write(0);   // Define a posição inicial do servo motor para 0 graus
+  lcd.init();
+  lcd.backlight();
 
   // Definição das Tarefas
-  xTaskCreate(tarefaPotServo, "tarefaPotServo", 80, NULL, 5, NULL);  // 1 - prioridade mín, 5 - prioridade máx // palavra = 1 byte // máx de palavras na memória = 1000
-  xTaskCreate(tarefaTempUmi, "tarefaTempUmi", 400, NULL, 4, NULL);
+  xTaskCreate(tarefaPotServo, "tarefaPotServo", 128, NULL, 5, NULL);  // 1 - prioridade mín, 5 - prioridade máx // palavra = 1 byte // máx de palavras na memória = 1000
+  xTaskCreate(tarefaTempUmi, "tarefaTempUmi", 450, NULL, 4, NULL);
+  xTaskCreate(exibirLCD, "exibirLCD", 140, NULL, 3, NULL);
 
   // Cria a tarefa de monitoramento
   //xTaskCreate(TaskMonitor, "Monitor", 128, NULL, 4, NULL);
-
-  //showMenu();
 }
 
 void loop() {
-  /*
-  if (!digitalRead(BTN_UP)) {
-    menuIndex = (menuIndex - 1 + menuSize) % menuSize;  // Navegar para cima
-    delay(200);                                         // Debounce
-    showMenu();
-  }
-
-  if (!digitalRead(BTN_DOWN)) {
-    menuIndex = (menuIndex + 1) % menuSize;  // Navegar para baixo
-    delay(200);                              // Debounce
-    showMenu();
-  }
-
-  if (!digitalRead(BTN_SELECT)) {
-    delay(200);  // Debounce
-    executeOption(menuIndex);
-    showMenu();
-  }
-}
-
-void showMenu() {
-  lcd.clear();
-  lcd.setCursor(0, 0);
-  lcd.print("-> ");
-  lcd.print(menuOptions[menuIndex]);  // Mostra a opção selecionada
-                                      //if (menuIndex + 1 < menuSize) { // Mostra a próxima opção, se existir
-  lcd.setCursor(0, 1);
-  //lcd.print("   ");
-  lcd.print(menuOptions[(menuIndex + 1) % menuSize]);
-  //}
-}
-
-void executeOption(int index) {
-  lcd.clear();
-  switch (index) {
-    case 0:  // Potenciômetro e Servo
-      while (true) {
-        val = analogRead(potPin);         // Lê o valor do potenciômetro (0 a 1023)
-        val = constrain(val, 0, 1023);    // Garante que o valor fique entre 0 e 1023
-        pos = map(val, 0, 1023, 0, 180);  // Mapeia o valor do potenciômetro para a faixa de 0 a 180 graus
-        meuServo.write(pos);              // Define a posição do servo motor
-        Serial.print("Pot: ");
-        Serial.print(val);
-        Serial.print(" / Pos: ");
-        Serial.println(pos);
-
-        lcd.setCursor(0, 0);  // Define o cursor na primeira linha
-        lcd.print("Potenc.: ");
-        lcd.print("       ");
-        lcd.setCursor(9, 0);
-        lcd.print(val);       // Exibe o texto formatado
-        lcd.setCursor(0, 1);  // Define o cursor na segunda linha
-        lcd.print("Pos. Servo: ");
-        lcd.print("    ");
-        lcd.setCursor(12, 1);
-        lcd.print(pos);  // Exibe o texto formatado
-
-        delay(15);  // Aguarda um curto intervalo para estabilizar o movimento do servo
-
-        if (!digitalRead(BTN_BACK)) break;  // Voltar ao menu
-      }
-      break;
-
-    case 1:  // Temperatura e Umidade
-      while (true) {
-        float temp = dht.readTemperature();
-        float hum = dht.readHumidity();
-        if (isnan(temp) || isnan(hum)) {
-          Serial.println(F("Failed to read from DHT sensor!")); //Por padrão, strings literais são armazenadas na RAM, mas usando F(), a string é mantida na memória de programa (flash), que geralmente é mais abundante que a RAM no Arduino.
-          return;
-        }
-        lcd.setCursor(0, 0);
-        lcd.print("Temp.: ");
-        lcd.print("         ");
-        lcd.setCursor(7, 0);
-        lcd.print(temp);
-        lcd.print(" C");
-        lcd.setCursor(0, 1);
-        lcd.print("Umid.: ");
-        lcd.print("         ");
-        lcd.setCursor(7, 1);
-        lcd.print(hum);
-        lcd.print(" %");
-        Serial.print("Umidade: ");
-        Serial.print(hum);
-        Serial.print(" / Temperatura: ");
-        Serial.print(temp);
-        Serial.println("°C");
-        if (!digitalRead(BTN_BACK)) break;  // Voltar ao menu
-      }
-      break;
-
-    case 2:  // Relé
-      releState = !releState;
-      digitalWrite(RELE_PIN, releState ? HIGH : LOW);
-      lcd.print(releState ? "Rele Ligado" : "Rele Desligado");
-      delay(1000);
-      break;
-
-    case 3:  // Mensagem no LCD
-      lcd.print("Digite no serial");
-      Serial.println("Digite para enviar ao LCD: ");
-
-      while (true) {
-        if (Serial.available()) {
-          // Lê o texto enviado pelo monitor serial
-          String texto = Serial.readStringUntil('\n');  // Lê até encontrar uma nova linha
-          texto.trim();                                 // Remove espaços ou quebras de linha desnecessárias
-
-          // Limpa o display LCD
-          lcd.clear();
-
-          // Exibe o texto no LCD
-          lcd.setCursor(0, 0);
-          if (texto.length() <= 16) {
-            // Texto cabe em uma linha
-            lcd.print(texto);
-          } else {
-            // Divide o texto em duas linhas
-            lcd.print(texto.substring(0, 16));  // Primeiros 16 caracteres
-            lcd.setCursor(0, 1);
-            lcd.print(texto.substring(16, 32));  // Próximos 16 caracteres
-          }
-
-          // Mensagem no monitor serial para indicar que o texto foi exibido
-          Serial.println("Texto enviado para o LCD!");
-        }
-        if (!digitalRead(BTN_BACK)) break;  // Voltar ao menu
-      }
-      break;
-
-    case 4:  // LED Vermelho
-      ledVermelhoState = !ledVermelhoState;
-      digitalWrite(LED_VERMELHO, ledVermelhoState ? HIGH : LOW);
-      lcd.print(ledVermelhoState ? "LED Vermelho On" : "LED Vermelho Off");
-      delay(1000);
-      break;
-
-    case 5:  // LED Amarelo
-      ledAmareloState = !ledAmareloState;
-      digitalWrite(LED_AMARELO, ledAmareloState ? HIGH : LOW);
-      lcd.print(ledAmareloState ? "LED Amarelo On" : "LED Amarelo Off");
-      delay(1000);
-      break;
-
-    case 6:  // Backlight
-      if (!backlightState) {
-        Serial.println("Backlight On");
-      } else Serial.println("Backlight Off");
-      backlightState = !backlightState;
-      backlightState ? lcd.backlight() : lcd.noBacklight();
-      lcd.print(backlightState ? "Backlight On" : "Backlight Off");
-      delay(1000);
-      break;
-  }
-*/
+ 
 }
 
 
 void tarefaPotServo() {
 
-  Servo meuServo;   // Cria um objeto Servo para controlar o servo motor
-  int pos;          // Variável para armazenar a posição do servo motor
-  int potPin = A0;  // Pino analógico onde o potenciômetro está conectado
-  int val;          // Variável para armazenar o valor lido do potenciômetro
+  Servo meuServo;  // Cria um objeto Servo para controlar o servo motor
+  //int pos;          // Variável para armazenar a posição do servo motor
+  //int val;          // Variável para armazenar o valor lido do potenciômetro
 
   meuServo.attach(6);  // Associa o servo motor ao pino digital 6 do Arduino
   meuServo.write(0);   // Define a posição inicial do servo motor para 0 graus
@@ -267,39 +66,13 @@ void tarefaPotServo() {
     val = constrain(val, 0, 1023);    // Garante que o valor fique entre 0 e 1023
     pos = map(val, 0, 1023, 0, 180);  // Mapeia o valor do potenciômetro para a faixa de 0 a 180 graus
     meuServo.write(pos);              // Define a posição do servo motor
-    
-    Serial.print("Pot: ");
-    Serial.print(val);
-    Serial.print(" / Pos: ");
-    Serial.println(pos);
-    /*
-    lcd.setCursor(0, 0);  // Define o cursor na primeira linha
-    lcd.print("Potenc.: ");
-    lcd.print("       ");
-    lcd.setCursor(9, 0);
-    lcd.print(val);       // Exibe o texto formatado
-    lcd.setCursor(0, 1);  // Define o cursor na segunda linha
-    lcd.print("Pos. Servo: ");
-    lcd.print("    ");
-    lcd.setCursor(12, 1);
-    lcd.print(pos);  // Exibe o texto formatado
-    */
 
-    //delay(15);  // Aguarda um curto intervalo para estabilizar o movimento do servo
+    //Serial.print("Pot: ");
+    //Serial.print(val);
+    //Serial.print(" / Pos: ");
+    //Serial.println(pos);
+
     vTaskDelay(pdMS_TO_TICKS(10));
-
-    for (;;) {
-    // Obtém o High Water Mark da tarefa "TaskExample"
-    UBaseType_t highWaterMark = uxTaskGetStackHighWaterMark(NULL);
-
-    // Calcula a memória utilizada (em palavras da pilha)
-    Serial.print("Espaço mínimo livre na pilha: ");
-    Serial.print(highWaterMark);
-    Serial.println(" palavras");
-
-    // Aguarda 2 segundos antes de monitorar novamente
-    vTaskDelay(pdMS_TO_TICKS(2000));
-  }
   }
 }
 
@@ -308,33 +81,52 @@ void tarefaTempUmi() {
 
   while (1) {
 
-    float temp = dht.readTemperature();
-    float hum = dht.readHumidity();
+    temp = dht.readTemperature();
+    umid = dht.readHumidity();
 
-    if (isnan(temp) || isnan(hum)) {
-      Serial.println(F("Failed to read from DHT sensor!"));  //Por padrão, strings literais são armazenadas na RAM, mas usando F(), a string é mantida na memória de programa (flash), que geralmente é mais abundante que a RAM no Arduino.
+    if (isnan(temp) || isnan(umid)) {
+      //Serial.println(F("Failed to read from DHT sensor!"));  //Por padrão, strings literais são armazenadas na RAM, mas usando F(), a string é mantida na memória de programa (flash), que geralmente é mais abundante que a RAM no Arduino.
       return;
     }
-    /*
-    Serial.print("Umidade: ");
-    Serial.print(hum);
-    Serial.print(" / Temperatura: ");
-    Serial.print(temp);
-    Serial.println("°C");
-    /*
+    
+    //Serial.print("Umidade: ");
+    //Serial.print(hum);
+    //Serial.print(" / Temperatura: ");
+    //Serial.print(temp);
+    //Serial.println("°C");
+    
+    vTaskDelay(pdMS_TO_TICKS(10));
+  }
+}
+
+
+// Tarefa de monitoramento (exibição no LCD)
+void exibirLCD(void *pvParameters) {
+  for (;;) {
+    // Exibir valores do potenciômetro e servo
     lcd.setCursor(0, 0);
-    lcd.print("Temp.: ");
-    lcd.print("         ");
-    lcd.setCursor(7, 0);
-    lcd.print(temp);
-    lcd.print(" C");
+    lcd.print("Pot:");
+    lcd.print("    ");
+    lcd.setCursor(4, 0);
+    lcd.print(val);
+    lcd.setCursor(8, 0);
+    lcd.print(" Pos:");
+    lcd.print("   ");
+    lcd.setCursor(13, 0);
+    lcd.print(pos);
+
+    // Exibir valores do temperatura e humidade
     lcd.setCursor(0, 1);
-    lcd.print("Umid.: ");
-    lcd.print("         ");
-    lcd.setCursor(7, 1);
-    lcd.print(hum);
-    lcd.print(" %");
-    */
+    lcd.print("Tem:");
+    lcd.print("    ");
+    lcd.setCursor(4, 1);
+    lcd.print(temp);
+    lcd.setCursor(8, 1);
+    lcd.print(" Umi:");
+    lcd.print("   ");
+    lcd.setCursor(13, 1);
+    lcd.print(umid);
+
     vTaskDelay(pdMS_TO_TICKS(10));
   }
 }
@@ -353,7 +145,4 @@ void TaskMonitor(void *pvParameters) {
     // Aguarda 2 segundos antes de monitorar novamente
     vTaskDelay(pdMS_TO_TICKS(2000));
   }
-}
-*/
-
-
+}*/
